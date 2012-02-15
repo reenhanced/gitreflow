47a300163ee4eb35aa2d2d34a30d605634ea37b1
Robert Stern <robert@reenhanced.com>
2012-02-15 08:54:20 -0500

    Implement git reflow start command.
    [Fixes #23673181]
    Closes #3
    
    Squashed commit of the following:
    
    commit 0c3a55b53c3d70ee2cf6a0f4e9e4ef99899dbb51
    Author: Robert Stern <robert@reenhanced.com>
    Date:   Wed Feb 15 08:42:19 2012 -0500
    
        Made step allow for alternate remote repositories.
    
    commit a56c264d602efe91f658f9898d4c0379603f2278
    Author: Valentino <valentino@reenhanced.com>
    Date:   Tue Feb 14 09:50:00 2012 -0500
    
        Refactored steps, and made changes for pull request
    
    commit c603529c57c8deba62afc472d04748e50b18d0da
    Author: Valentino <valentino@reenhanced.com>
    Date:   Mon Feb 13 09:25:43 2012 -0500
    
        Added final step to setup remote tracking for new feature branch
    
    commit ba8f3c6e1971c891f0843145edd97164a918cae8
    Author: Valentino <valentino@reenhanced.com>
    Date:   Mon Feb 13 09:10:11 2012 -0500
    
        Made output matchers regex
    
    commit b0f0326fe04c905aa5e63f750ee94870a1f55af3
    Merge: 8de65ca dc5eda1
    Author: Valentino <valentino@reenhanced.com>
    Date:   Fri Feb 10 10:22:52 2012 -0500
    
        Merge remote-tracking branch 'origin/start-command' into start-command
    
    commit 8de65cae8245d417f415dd90fe5c757129747e32
    Author: Valentino <valentino@reenhanced.com>
    Date:   Fri Feb 10 10:22:16 2012 -0500
    
        Tests pass for 'git fetch origin'
    
    commit 8e1b241c0a0902e768b27207c4830ddb7729c4be
    Author: Valentino <valentino@reenhanced.com>
    Date:   Thu Feb 9 10:20:36 2012 -0500
    
        Started on git fetch test
    
    commit dc5eda175a720b87d1c0c41317be27589f0a2795
    Author: Robert Stern <robert@reenhanced.com>
    Date:   Thu Feb 9 09:58:28 2012 -0500
    
        Remove spaces.
    
    commit ac6b6b7bc25926bc55956b7b3b0a4bd11ee6328f
    Author: Valentino <valentino@reenhanced.com>
    Date:   Thu Feb 9 09:55:15 2012 -0500
    
        Changed readme
    
    commit e8931cd39951468e91949b1aaf6bc81b2c0ebdf8
    Author: Valentino <valentino@reenhanced.com>
    Date:   Thu Feb 9 09:54:51 2012 -0500
    
        Changed readme
    
    commit f74beb8e90779458829d3ff9866bfa0bc7a95c1c
    Author: Valentino <valentino@reenhanced.com>
    Date:   Thu Feb 9 09:41:53 2012 -0500
    
        Added new test for git fetch
    
    commit bc902487b93ead4b0c87bf7868072f3acbfcaeac
    Author: Valentino <valentino@reenhanced.com>
    Date:   Thu Feb 9 09:23:34 2012 -0500
    
        Got the fucking tests working with git repositories
    
    commit d1fd27923a0d71f33ab16d8b7fd736c6974c2eca
    Author: Robert Stern <robert@reenhanced.com>
    Date:   Wed Feb 8 09:48:25 2012 -0500
    
        Got tmp directories working
    
    commit d76d10e188c66855899f734be880c8066f18c53d
    Author: Robert <robert@macpro.local>
    Date:   Wed Feb 8 08:59:06 2012 -0500
    
        Initial commit
    
    commit 8708ca62af71cdc5d24f72134afed546ebd905ff
    Author: Valentino <valentino@reenhanced.com>
    Date:   Wed Feb 8 08:56:01 2012 -0500
    
        Initial fixes to gem building for multiple scenarios
    
    commit c8c024e14bfd6eae8feb7efbb8e3fda00efe6655
    Author: Valentino <v@valentino.local>
    Date:   Tue Feb 7 10:09:49 2012 -0500
    
        Initial commit
    
    commit 3c1fa928127336eb33433f92383a583c1fac063d
    Author: Valentino <v@valentino.local>
    Date:   Tue Feb 7 09:32:29 2012 -0500
    
        Initial commit
    
    commit bb8e91e44f0748ff05e12746316a72af36f7b907
    Author: Valentino <valentino@reenhanced.com>
    Date:   Tue Feb 7 09:24:43 2012 -0500
    
        Remove mistakenly committed file.
    
    commit cde1ec52ffc5db273a87f6b5c443599f9b39d1c9
    Author: Valentino <valentino@reenhanced.com>
    Date:   Tue Feb 7 09:24:21 2012 -0500
    
        Added .gitignore.
    
    commit 8d2c5329e6e15d0ad06c84d955743ff47007e7b6
    Author: Valentino <v@valentino.local>
    Date:   Tue Feb 7 08:57:04 2012 -0500
    
        Initial commit
    
    commit 164d80dba496896569e4110b3da9701f472dd54a
    Author: Valentino <valentino@reenhanced.com>
    Date:   Wed Feb 1 10:15:16 2012 -0500
    
        Added steps to setup mock repos for git testing

---
+++ features/step_definitions/git_steps.rb
@@ -0,0 +1,69 @@
+Given /^I have a git repository with a branch named "([^"]+)" checked out$/ do |branch_name|
+  steps %{
+    Given a directory named "master_repo"
+    And I cd to "master_repo"
+    And I write to "README" with:
+      | empty file |
+    And I successfully run `git init`
+    And I successfully run `git add README`
+    And I successfully run `git commit -m "Initial commit"`
+  }
+
+  unless branch_name == "master"
+    steps %{
+      And I successfully run `git checkout -b #{branch_name}`
+    }
+  end
+
+  steps %{
+    And I cd to ".."
+  }
+end
+
+Given /^I have a remote git repository named "([^"]+)"$/ do |remote_name|
+  steps %{
+    Given a directory named "#{remote_name}_repo"
+    When I cd to "#{remote_name}_repo"
+    And I successfully run `git init`
+    And I write to "README" with:
+      | Initialized |
+    And I successfully run `git add .`
+    And I successfully run `git commit -am "Initial commit"`
+    And I cd to ".."
+    And I cd to "master_repo"
+    And I successfully run `git remote add #{remote_name} ../#{remote_name}_repo`
+    And I cd to ".."
+  }
+end
+
+Given /^the remote repository named "([^"]+)" has changes on the "([^"]+)" branch$/ do |remote_name, branch_name|
+  steps %{
+    Given a directory named "#{remote_name}_repo"
+    When I cd to "#{remote_name}_repo"
+    And I successfully run `git checkout master`
+    And I append to "README" with:
+      | changed |
+    And I successfully run `git add .`
+    And I successfully run `git commit -am "Changed readme"`
+    And I cd to ".."
+  }
+end
+
+Given /^the repository has been initialized$/ do
+  steps %{
+    Given I successfully run `git branch`
+    Then the output should contain "master"
+  }
+end
+
+Then /^a branch named "([^"]+)" should have been created from "([^"]+)"$/ do |new_branch, base_branch|
+  steps %{
+    Then the output should match /\\* \\[new branch\\]\\s* #{Regexp.escape(base_branch)}\\s* \\-\\> #{Regexp.escape(new_branch)}/
+  }
+end
+
+Then /^the base branch named "([^"]+)" should have fetched changes from the remote git repository "([^"]+)"$/ do |base_branch, remote_name|
+  steps %{
+    Then the output should match /\\* \\[new branch\\]\\s* #{Regexp.escape(base_branch)}\\s* \\-\\> #{remote_name}.#{Regexp.escape(base_branch)}/
+  }
+end

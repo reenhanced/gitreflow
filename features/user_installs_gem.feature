66d161b57dc991e17cad70ca941ec9e276271e77
Valentino <valentino@reenhanced.com>
2012-04-05 09:27:17 -0400

    As a user, when I setup for the first time, it creates and stores my GitHub oath token
    Fixes #27452707
    Closes #4
    
    Squashed commit of the following:
    
    commit d6b9bc2b500a614935aab53f8004c7eb2321b53a
    Author: Valentino <valentino@reenhanced.com>
    Date:   Thu Apr 5 09:26:47 2012 -0400
    
        specs passing
    
    commit 8a002dc48192e086a161f17a906ab67b43b4b394
    Author: Valentino <valentino@reenhanced.com>
    Date:   Thu Apr 5 09:21:04 2012 -0400
    
        removed unused @announce from deliver feature
    
    commit 8919fb3d3d11cc4aa93a6a0459607ff10375ecaa
    Author: Valentino <valentino@reenhanced.com>
    Date:   Wed Apr 4 10:16:30 2012 -0400
    
        Refactored for pull request comments
    
    commit ac70beaee4c1afcf0d1cdfef53f01b8932788487
    Author: Valentino <valentino@reenhanced.com>
    Date:   Tue Apr 3 10:33:42 2012 -0400
    
        As a user, when I setup for the first time, it creates and stores my GitHub oath token
    
    commit 5c5b75c5395f3897958514d46744fcc1b0384fd9
    Author: Valentino <valentino@reenhanced.com>
    Date:   Tue Apr 3 09:22:51 2012 -0400
    
        Added Github authorization setup
    
    commit 177615a59fe16c8cfe4bf5f88b3c614008ceb111
    Author: Valentino <valentino@reenhanced.com>
    Date:   Mon Apr 2 09:59:22 2012 -0400
    
        Rspec for setup command WIP
    
    commit c628edd522c0be8b87a9caffd0c44009e901e75f
    Author: Valentino <valentino@reenhanced.com>
    Date:   Fri Mar 23 09:51:47 2012 -0400
    
        Switch to use github_api gem over github gem
    
    commit d7898cb152a434b94ab5aaa0af895ffd3c181535
    Author: Valentino <valentino@reenhanced.com>
    Date:   Wed Mar 21 09:37:29 2012 -0400
    
        Started testing subcommands
    
    commit af2b87fca775371c82a7b831616d7f31f38fb528
    Author: Valentino <valentino@reenhanced.com>
    Date:   Wed Feb 15 09:45:00 2012 -0500
    
        Added initial features for initiating pull requests

--- features/user_installs_gem.feature
+++ features/user_installs_gem.feature
@@ -5,5 +5,14 @@ Feature: User installs gem
 
   Scenario: User installs gem
     When I build and install the gem
-    And I successfully run `git-reflow`
+    Then the output should contain "You need to setup your GitHub OAuth token\nPlease run 'git-reflow setup'"
+    When I successfully run `git-reflow`
     Then the output should contain "usage: git-reflow [global options] command [command options]"
+
+  Scenario: User sets up GitHub
+    When I run `git-reflow setup` interactively
+    And I type "user"
+    And I type "password"
+    Then the output should contain "Please enter your GitHub username: "
+    And the output should contain "Please enter your GitHub password (we do NOT store this): "
+    And the output should contain "Your GitHub account was successfully setup!"
--- lib/git_reflow.rb
+++ lib/git_reflow.rb
@@ -3,9 +3,31 @@ require 'rake'
 require 'json'
 require 'open-uri'
 require 'httpclient'
-
-require 'git_reflow/base'
+require 'github_api'
 
 module GitReflow
+  extend self
 
+  def setup
+    print "Please enter your GitHub username: "
+    gh_user = STDIN.gets.chomp
+    `stty -echo`
+    print "Please enter your GitHub password (we do NOT store this): "
+    gh_password = STDIN.gets.chomp
+    `stty echo`
+    puts "\nYour GitHub account was successfully setup!"
+    github = Github.new :basic_auth => "#{gh_user}:#{gh_password}"
+    authorization = github.oauth.create_authorization 'scopes' => ['repo']
+    oauth_token = authorization[:token]
+    set_oauth_token(oauth_token)
+  end
+
+  def get_oauth_token
+    `git config --get github.oauth-token`.strip
+  end
+
+  private
+  def set_oauth_token(oauth_token)
+    `git config --global --add github.oauth-token #{oath_token}`
+  end
 end

ab19e95e50f1ac4b87505cc0e2cbc3f5bbbf655c
Valentino <valentino@reenhanced.com>
2012-05-15 10:01:21 -0400

    Deliver checks out destination branch before merging
    [Fixes #23813477]
    Closes #19
    
    Squashed commit of the following:
    
    commit 16096a6b27be6f44f1171a8a3b32db587d4ef866
    Author: Valentino <valentino@reenhanced.com>
    Date:   Tue May 15 09:53:08 2012 -0400
    
        [fix] Autoloading spec support files, and Hash hack...
    
    commit 698114397117754fcb41bf6b9bfc314a2874fa6e
    Author: Valentino <valentino@reenhanced.com>
    Date:   Tue May 15 09:39:38 2012 -0400
    
        Refactored github helpers
    
    commit 6c9c80da63a5d0ce8db71fbc62c7cd695c85010a
    Author: Valentino <valentino@reenhanced.com>
    Date:   Tue May 15 09:07:49 2012 -0400
    
        Removed commented line
    
    commit 3fb7ad7a9b7db3ac8ed0b4a3a4f3829a1a6a69be
    Author: Valentino <valentino@reenhanced.com>
    Date:   Tue May 15 09:07:18 2012 -0400
    
        Refactored features to not use spec helper
    
    commit c786119a2c5e86f18316b888617f2ac5ae1abb94
    Author: Valentino <valentino@reenhanced.com>
    Date:   Mon May 14 10:22:26 2012 -0400
    
        Refactored based on pull request comments
    
    commit c2f7d9ae6a01f94cb9a828e9583adba0b2b4fcab
    Author: Valentino <valentino@reenhanced.com>
    Date:   Mon May 14 09:54:55 2012 -0400
    
        "fixed" feature to mock current branch
    
    commit 9a5af4d50ffa6e71473f4847e270d08ec9aedca2
    Author: Valentino <valentino@reenhanced.com>
    Date:   Mon May 14 09:52:09 2012 -0400
    
        Checked out base branch on deliver
    
    commit b1188d3e4a7672bc3865643abab09293a7a4d88d
    Author: Valentino <valentino@reenhanced.com>
    Date:   Mon May 14 09:42:07 2012 -0400
    
        TEST FUCKING PASSING, TRIPPLE EXCLAMATION MARKS
    
    commit d990c0571f91965108eb7a9ae24d5711f5098f74
    Author: Valentino <valentino@reenhanced.com>
    Date:   Fri May 11 10:41:38 2012 -0400
    
        WIP - found we need to use custom step to run commands with stubs
    
    commit 6f2be144234783fc83fbe5821c286c8585f283de
    Author: Valentino <valentino@reenhanced.com>
    Date:   Thu May 10 10:24:20 2012 -0400
    
        WIP - debugging hidden undefined method in deliver feature
    
    commit 70aaf0195267897abe3ccdf28a7b34b49567b22d
    Author: Valentino <valentino@reenhanced.com>
    Date:   Wed May 9 10:30:20 2012 -0400
    
        WIP - Added initial stubbing for Github
    
    commit cb92c0a581f8e5a49b617947547a00a03dae47c0
    Author: Valentino <valentino@reenhanced.com>
    Date:   Tue May 8 10:03:39 2012 -0400
    
        WIP - started features for checking out destination branch
    
    commit e1aa95f353cb2791b7eb36c758307d2da27dd5b8
    Author: Valentino <valentino@reenhanced.com>
    Date:   Tue May 8 10:03:25 2012 -0400
    
        WIP - started features for checking out destination branch

---
+++ features/user_delivers_flow.feature
@@ -0,0 +1,21 @@
+@gem
+Feature: User delivers a flow
+  As a User
+  I can deliver a flow
+  So I can merge in my topic branch
+
+  Background:
+    Given I have a git repository with a branch named "master" checked out
+    And I have a remote git repository named "origin"
+    And the remote repository named "origin" has changes on the "master" branch
+    And I cd to "master_repo"
+    When I run `git-reflow start new-branch`
+    And I append to "README" with:
+      | changed |
+    And I successfully run `git add .`
+    And I successfully run `git commit -am "Changed readme"`
+    Given I have a reviewed feature branch named "new-branch" checked out
+
+  Scenario: User runs git-reflow deliver without any parameters
+    When I deliver my "new-branch" branch
+    Then the branch "master" is checked out
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,7 +8,10 @@ $LOAD_PATH << 'lib'
 require 'git_reflow'
 require 'github_api'
 
+Dir[File.expand_path('../support/**/*.rb', __FILE__)].each {|f| require f}
+
 RSpec.configure do |config|
+  config.include GithubHelpers
   config.include WebMock::API
   config.color_enabled = true
   config.before(:each) do
@@ -16,68 +19,10 @@ RSpec.configure do |config|
   end
 end
 
-def stub_get(path, endpoint = Github.endpoint.to_s)
-  stub_request(:get, endpoint + path)
-end
-
-def stub_post(path, endpoint = Github.endpoint.to_s)
-  stub_request(:post, endpoint + path)
-end
-
-def stub_patch(path, endpoint = Github.endpoint.to_s)
-  stub_request(:patch, endpoint + path)
-end
-
-def stub_put(path, endpoint = Github.endpoint.to_s)
-  stub_request(:put, endpoint + path)
-end
-
-def stub_delete(path, endpoint = Github.endpoint.to_s)
-  stub_request(:delete, endpoint + path)
-end
-
-def a_get(path, endpoint = Github.endpoint.to_s)
-  a_request(:get, endpoint + path)
-end
-
-def a_post(path, endpoint = Github.endpoint.to_s)
-  a_request(:post, endpoint + path)
-end
-
-def a_patch(path, endpoint = Github.endpoint.to_s)
-  a_request(:patch, endpoint + path)
-end
-
-def a_put(path, endpoint = Github.endpoint)
-  a_request(:put, endpoint + path)
-end
-
-def a_delete(path, endpoint = Github.endpoint)
-  a_request(:delete, endpoint + path)
-end
-
-def fixture_path
-  File.expand_path("../fixtures", __FILE__)
-end
-
-def fixture(file)
-  File.new(File.join(fixture_path, '/', file))
-end
-
 OAUTH_TOKEN = 'bafec72922f31fe86aacc8aca4261117f3bd62cf'
 
 def reset_authentication_for(object)
   [ 'user', 'repo', 'basic_auth', 'oauth_token', 'login', 'password' ].each do |item|
     object.send("#{item}=", nil)
-  end
-end
-
-# the github_api gem does some overrides to Hash so we have to make sure
-# this still works here...
-class Hash
-  def except(*keys)
-    cpy = self.dup
-    keys.each { |key| cpy.delete(key) }
-    cpy
   end
 end

4d1d1ed33b0541a2188a28526179703b13dd57b0
Valentino <valentino@reenhanced.com>
2012-09-25 09:46:02 -0400

    Add switch to skip lgtm checks on deliver
    [Deliver #34870873]
    Closes #32
    
    LGTM given by: @armyofgnomes
    
    Squashed commit of the following:
    
    commit 81eff4b7bb0a43007a2cfa27ecdb56c4ac131685
    Author: Valentino <valentino@reenhanced.com>
    Date:   Tue Sep 25 09:30:26 2012 -0400
    
        cleanup help message for skip-lgtm switch
    
    commit a6fba34897c78c67a14dc2a8b1f07c818757683d
    Author: Valentino <valentino@reenhanced.com>
    Date:   Tue Sep 25 09:27:32 2012 -0400
    
        Add switch to skip lgtm checks on deliver

--- lib/git_reflow/commands/deliver.rb
+++ lib/git_reflow/commands/deliver.rb
@@ -2,6 +2,8 @@ desc 'deliver your feature branch'
 long_desc 'merge your feature branch down to your base branch, and cleanup your feature branch'
 
 command :deliver do |c|
+  c.desc 'skip the lgtm checks and deliver your feature branch'
+  c.switch [:f, :'skip-lgtm']
   c.desc 'merge your feature branch down to your base branch, and cleanup your feature branch'
   c.arg_name 'base_branch - the branch you want to merge into'
   c.action do |global_options,options,args|
@@ -13,6 +15,7 @@ command :deliver do |c|
     when 1
       deliver_options['base'] = args[0]
     end
+    deliver_options.merge({'skip_lgtm' => options[:'skip-lgtm']})
     GitReflow.deliver deliver_options
   end
 end

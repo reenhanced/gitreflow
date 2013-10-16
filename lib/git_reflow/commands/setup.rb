800874d78843721e88b7c5044c31b963525b6785
Valentino <valentino@reenhanced.com>
2013-10-16 08:35:59 -0400

    Enable use of Github Enterprise, and fix review command flags
    Closes #46, #41, #42, #43, #44
    
    LGTM given by: @nhance
    
    Squashed commit of the following:
    
    commit 0e10c209bde4be993c59e2b0fce68982a1bb2f16
    Author: Valentino <valentino@reenhanced.com>
    Date:   Wed Oct 16 08:35:37 2013 -0400
    
        Update fetching of comments for new API
    
    commit 0457f72b5cf8d6a9ac1e6ade8b123766f3dec9cd
    Author: Valentino <valentino@reenhanced.com>
    Date:   Wed Oct 16 08:15:54 2013 -0400
    
        Include pull request title in squashed merge message
    
    commit 99e1dd6a271e9a4b426bdb5922aaca5b87cbf29c
    Author: Valentino <valentino@reenhanced.com>
    Date:   Wed Oct 16 08:11:24 2013 -0400
    
        Cleanup duplicate content in readme, and add a LGTM option
    
    commit 51c34061db0c0f170a2813064c0addc7b715963e
    Author: Valentino <valentino@reenhanced.com>
    Date:   Tue Oct 15 15:16:55 2013 -0400
    
        [fix] Update to github_api gem caused issue merging comments
    
    commit 6822f7888d8ae75e7acf6f9c52be37f9f1ea2ab2
    Author: Valentino <valentino@reenhanced.com>
    Date:   Tue Oct 15 15:07:52 2013 -0400
    
        Updated Readme for new setup command options for local and enterprise
    
    commit 8a79ffd1dac6936a5252c91f03ef028dd41afe73
    Author: Valentino <valentino@reenhanced.com>
    Date:   Tue Oct 15 11:07:39 2013 -0400
    
        Initial additions to setup command for project-specific and enterprise
    
    commit 1a15364c639fb8b5ffe5f0c96926a566bfe770ff
    Author: Valentino <valentino@reenhanced.com>
    Date:   Tue Oct 15 08:36:00 2013 -0400
    
        Refactor for pull request comments
    
    commit eb161108b63b123fdce446f72e3857bf810a1805
    Author: Valentino <valentino@reenhanced.com>
    Date:   Tue Oct 15 08:11:59 2013 -0400
    
        [fix] Review flags not being passed
    
    commit d2097cc59786f1456bc912d285dac5fe2a78486f
    Author: Valentino <valentino@reenhanced.com>
    Date:   Thu Oct 10 10:37:48 2013 -0400
    
        [fix] need to move review flags into scope of command
    
    commit 7fa5e39e0c8c955e3e7a718197d672ea7ac42020
    Author: Valentino <valentino@reenhanced.com>
    Date:   Thu Oct 10 10:17:26 2013 -0400
    
        Don't yet upgrade github_api gem
    
    commit 3a9dca0bc751a2f5923a83f498ff301a08c0f4e6
    Author: Valentino <valentino@reenhanced.com>
    Date:   Thu Oct 10 09:09:50 2013 -0400
    
        Updated readme with instructions on setting up GitHub Enterprise accounts
    
    commit 7842156dc88134413992acd73dbcbdeb2fb485db
    Merge: aec25db a8ca369
    Author: Valentino <valentino@reenhanced.com>
    Date:   Thu Oct 10 08:55:28 2013 -0400
    
        Merge remote-tracking branch 'origin/master' into vs-github-enterprise
    
    commit aec25db80cca37543da906a9d2f9cf8c66104e18
    Author: Valentino <valentino@reenhanced.com>
    Date:   Thu Oct 10 08:53:35 2013 -0400
    
        [fix] Github endpoint and site urls, review command using custom title and message flags, and upgrade gems
    
    commit 7b7b706f6db3f9038846ef6698cf3df5c6e5dec6
    Author: Valentino <valentino@reenhanced.com>
    Date:   Wed Oct 9 14:26:13 2013 -0400
    
        Display git commands as they run, and add flags for review title and message
    
    commit 2d4cda55f9a8872d04dee76ad3556a0200ee20fb
    Author: Valentino <valentino@reenhanced.com>
    Date:   Tue Oct 1 14:34:23 2013 -0400
    
        Enable use of Github Enterprise accounts

--- lib/git_reflow/commands/setup.rb
+++ lib/git_reflow/commands/setup.rb
@@ -1,7 +1,9 @@
 desc 'Setup your GitHub account'
 command :setup do |c|
   c.desc 'sets up your api token with GitHub'
+  c.switch [:l, :local], default_value: false, desc: 'setup GitReflow for the current project only'
+  c.switch [:e, :enterprise], default_value: false, desc: 'setup GitReflow with a Github Enterprise account'
   c.action do |global_options, options, args|
-    GitReflow.setup
+    GitReflow.setup({ project_only: options[:local], enterprise: options[:enterprise] })
   end
 end

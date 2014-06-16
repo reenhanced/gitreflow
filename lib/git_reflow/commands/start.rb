4e971ff5a3bcb04782d9850c285202c7abe8fa60
Samuel Esposito <s.esposito@roqua.nl>
2014-06-16 15:47:02 +0200

    Update long_desc as well

--- lib/git_reflow/commands/start.rb
+++ lib/git_reflow/commands/start.rb
@@ -2,7 +2,7 @@ desc 'Start will create a new feature branch and setup remote tracking'
 long_desc <<LONGTIME
   Performs the following:\n
   \t$ git pull origin <current_branch>\n
-  \t$ git push origin master:refs/heads/[new_feature_branch]\n
+  \t$ git push origin <current_branch>:refs/heads/[new_feature_branch]\n
   \t$ git checkout --track -b [new_feature_branch] origin/[new_feature_branch]\n
 LONGTIME
 arg_name '[new-feature-branch-name] - name of the new feature branch'

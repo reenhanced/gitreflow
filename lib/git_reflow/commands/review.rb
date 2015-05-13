ea9b52200b6443e29446f0ff0fb6dcb3374ffcbc
Russell Jennings <violentpurr@gmail.com>
2015-05-13 18:51:54 -0400

    [lib] updates review.rb to open editor and adds -e option to edit the message that would normally be commited

--- lib/git_reflow/commands/review.rb
+++ lib/git_reflow/commands/review.rb
@@ -5,15 +5,38 @@ command :review do |c|
   c.arg_name '[destination_branch] - the branch you want to merge your feature branch into'
   c.flag [:t, :title], default_value: 'last commit message'
   c.flag [:m, :message], default_value: 'title'
+  c.flag [:e, :edit], default_value: "edit the auto generated commit"
   c.action do |global_options,options,args|
-    review_options = {
-      'base' => args[0],
-      'title' => global_options[:title],
-      'body' => global_options[:message]
-    }
 
-    review_options['title'] ||=  GitReflow.get_first_commit_message
-    review_options['body']  ||=  review_options['title']
+    commit_msg_file = '/tmp/git_reflow_pr_msg'
+
+    if global_options[:title] || global_options[:message]
+      review_options = {
+        'base' => args[0],
+        'title' => (global_options[:title]   || GitReflow.get_first_commit_message),
+        'body' =>  (global_options[:message] || GitReflow.get_first_commit_message)
+      }
+      edit = false
+    else # if edit or nothing provided, open editor
+      edit = true
+    end
+
+    if global_options[:edit]
+    File.open(commit_msg_file, 'w') do |file|
+      file.write(GitReflow.get_first_commit_message)
+    end
+
+    if edit
+      system('nano', commit_msg_file)
+      pr_msg = File.open(commit_msg_file).lines.map(&:strip).to_a
+      title = pr_msg.take(1)
+      File.delete('/tmp/reflow_pr_msg')
+      review_options = {
+        'base' => args[0],
+        'title' => title,
+        'body' =>  pr_msg
+      }
+    end
 
     GitReflow.review review_options
   end

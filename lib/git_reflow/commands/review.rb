0fa557f58341e576cc0d01cf6cc256a516f9e86e
Russell Jennings <violentpurr@gmail.com>
2015-05-14 17:00:07 -0400

    updates to use whatever the user has defined as

--- lib/git_reflow/commands/review.rb
+++ lib/git_reflow/commands/review.rb
@@ -5,36 +5,31 @@ command :review do |c|
   c.arg_name '[destination_branch] - the branch you want to merge your feature branch into'
   c.flag [:t, :title], default_value: 'last commit message'
   c.flag [:m, :message], default_value: 'title'
-  c.switch [:e, :edit]
   c.action do |global_options,options,args|
 
     pull_request_msg_file = '.git/GIT_REFLOW_PR_MSG'
+    editor = true
 
     if global_options[:title] || global_options[:message]
       review_options = {
         'base' => args[0],
         'title' => (global_options[:title]   || GitReflow.current_branch),
-        'body' =>  (global_options[:message] || GitReflow.get_first_commit_message)
+        'body' =>  global_options[:message]
       }
       editor = false
-    else # if edit flag or nothing is provided, open editor
-      editor = true
-    end
-
-    if options[:edit] # if edit we inject title & body into file before opening
-      File.open(pull_request_msg_file, 'w') do |file|
-        file.write(GitReflow.current_branch)
-        file.write("\n")
-        file.write(GitReflow.get_first_commit_message)
-      end
     end
 
     if editor
-      GitReflow.run("$EDITOR #{pull_request_msg_file}")
-      system('nano', pull_request_msg_file)
+      File.open(pull_request_msg_file, 'w') do |file|
+        file.write(GitReflow.current_branch)
+      end
+      GitReflow.run("$EDITOR #{pull_request_msg_file}", with_system: true)
       pr_msg = File.open(pull_request_msg_file).each_line.map(&:strip).to_a
-      title = pr_msg.shift
       File.delete(pull_request_msg_file)
+      title = pr_msg.shift
+      unless pr_msg.empty? 
+        pr_msg.shift if pr_msg.first.empty?
+      end
       review_options = {'base' => args[0],'title' => title,'body' =>  pr_msg.join("\n")}
     end
 
@@ -43,6 +38,6 @@ command :review do |c|
     puts "Title:\n#{review_options['title']}\n\n"
     puts "Body:\n#{review_options['body']}\n"
     puts "--------\n"
-    GitReflow.review(review_options) unless ask("Submit pull request? [Y/n]") =~ /n/i
+    GitReflow.review(review_options) unless ask("Submit pull request?(Y|n)") =~ /n/i
   end
 end

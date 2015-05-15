c1a2ef862478ecd144f85d9b596ff3f61233392e
Russell Jennings <violentpurr@gmail.com>
2015-05-15 14:39:13 -0400

    Updates review command to interactively set pull request title and body
    LGTM given by: @codenamev, @nhance
    Closes #99
    
    Squashed commit of the following:
    
    commit c5d7be765832744a99b414bf652a64f1f1cebf5e
    Author: Russell Jennings <violentpurr@gmail.com>
    Date:   Fri May 15 13:51:46 2015 -0400
    
          bumps version to 0.6.0
    
    commit 4fa4e754f9d59888e3fceb4cceaa136017aa9770
    Author: Russell Jennings <violentpurr@gmail.com>
    Date:   Fri May 15 13:44:39 2015 -0400
    
          updates review command logic, and updates to use git root directory
    
    commit 92ef82cd3c0df423621efb631a06b7dc33f2daf0
    Author: Russell Jennings <violentpurr@gmail.com>
    Date:   Fri May 15 13:43:59 2015 -0400
    
          updates readme to remove -e documentation, no longer valid
    
    commit d7ef89126478347133d890c6b7e2ac9f75f62eac
    Author: Russell Jennings <violentpurr@gmail.com>
    Date:   Thu May 14 17:01:34 2015 -0400
    
          changes prompt question to only show what enter will do; clearer behavior, and the opposite is inferred
    
    commit 0fa557f58341e576cc0d01cf6cc256a516f9e86e
    Author: Russell Jennings <violentpurr@gmail.com>
    Date:   Thu May 14 17:00:07 2015 -0400
    
          updates to use whatever the user has defined as
    
    commit f9882df352d3c0cf4a3ebf45e5bfa37733c5138d
    Author: Russell Jennings <violentpurr@gmail.com>
    Date:   Thu May 14 15:49:28 2015 -0400
    
          [cmd] updates code per PR
        uses ask instead of custom prompting
        renames variable commit_msg_file to pull_request_msg_file
    
    commit 3c11bdde6a940b0b639e510bff7594bd1d5f69c3
    Author: Russell Jennings <violentpurr@gmail.com>
    Date:   Wed May 13 19:51:42 2015 -0400
    
          [lib] expands on review functionality and adds ability to exit
    
    commit 727594b03d7e471949d5d3e8eb51210370c90ebf
    Author: Russell Jennings <violentpurr@gmail.com>
    Date:   Wed May 13 19:50:53 2015 -0400
    
          [doc] updatyes README with new workflow info
    
    commit 970c73c7e3aa1977f88ae72f33df19459a3910ab
    Author: Russell Jennings <violentpurr@gmail.com>
    Date:   Wed May 13 19:15:35 2015 -0400
    
          [lib] updates review command
    
    commit 48c60eab615a0768b5d2163e6524a26b6b816b8a
    Author: Russell Jennings <violentpurr@gmail.com>
    Date:   Wed May 13 18:54:18 2015 -0400
    
          [lib] fixes missing end
    
    commit ea9b52200b6443e29446f0ff0fb6dcb3374ffcbc
    Author: Russell Jennings <violentpurr@gmail.com>
    Date:   Wed May 13 18:51:54 2015 -0400
    
          [lib] updates review.rb to open editor and adds -e option to edit the message that would normally be commited
    
    commit be491dcdd4f5bdfa5788860c152080d2916483f6
    Author: Russell Jennings <violentpurr@gmail.com>
    Date:   Wed May 13 18:51:18 2015 -0400
    
          [version] bumps from 0.5.2 to 0.6.0 per major.minor.bugfix methodology

--- lib/git_reflow/commands/review.rb
+++ lib/git_reflow/commands/review.rb
@@ -6,15 +6,34 @@ command :review do |c|
   c.flag [:t, :title], default_value: 'last commit message'
   c.flag [:m, :message], default_value: 'title'
   c.action do |global_options,options,args|
-    review_options = {
-      'base' => args[0],
-      'title' => global_options[:title],
-      'body' => global_options[:message]
-    }
+    git_root_dir = run('git rev-parse --show-toplevel').strip
+    pull_request_msg_file = "#{git_root_dir}/.git/GIT_REFLOW_PR_MSG"
 
-    review_options['title'] ||=  GitReflow.get_first_commit_message
-    review_options['body']  ||=  review_options['title']
+    if global_options[:title] || global_options[:message]
+      review_options = {
+        'base' => args[0],
+        'title' => (global_options[:title]   || GitReflow.current_branch),
+        'body' =>  global_options[:message]
+      }
+    else
+      File.open(pull_request_msg_file, 'w') do |file|
+        file.write(GitReflow.current_branch)
+      end
+      GitReflow.run("$EDITOR #{pull_request_msg_file}", with_system: true)
+      pr_msg = File.open(pull_request_msg_file).each_line.map(&:strip).to_a
+      File.delete(pull_request_msg_file)
+      title = pr_msg.shift
+      unless pr_msg.empty? 
+        pr_msg.shift if pr_msg.first.empty?
+      end
+      review_options = {'base' => args[0],'title' => title,'body' =>  pr_msg.join("\n")}
+    end
 
-    GitReflow.review review_options
+    puts "\nReview your PR:\n"
+    puts "--------\n"
+    puts "Title:\n#{review_options['title']}\n\n"
+    puts "Body:\n#{review_options['body']}\n"
+    puts "--------\n"
+    GitReflow.review(review_options) unless ask("Submit pull request? (Y)") =~ /n/i
   end
 end

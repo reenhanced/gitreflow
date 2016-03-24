d5f53403698e29c37697a9e5b9ba44b8416b3559
Valentino Stoll <thevalentino+code@gmail.com>
2016-03-24 09:26:14 -0400

    Only add .gitconfig.reflow as a git include in the global scope

--- lib/git_reflow/commands/setup.rb
+++ lib/git_reflow/commands/setup.rb
@@ -4,12 +4,13 @@ command :setup do |c|
   c.switch [:l, :local], default_value: false, desc: 'setup GitReflow for the current project only'
   c.switch [:e, :enterprise], default_value: false, desc: 'setup GitReflow with a Github Enterprise account'
   c.action do |global_options, options, args|
-    reflow_options = { project_only: options[:local], enterprise: options[:enterprise] }
+    reflow_options             = { project_only: options[:local], enterprise: options[:enterprise] }
+    existing_git_include_paths = GitReflow::Config.get('include.path', all: true).split("\n")
 
-    unless File.exist?(GitReflow::Config::CONFIG_FILE_PATH)
+    unless File.exist?(GitReflow::Config::CONFIG_FILE_PATH) or existing_git_include_paths.include?(GitReflow::Config::CONFIG_FILE_PATH)
       GitReflow.run "touch #{GitReflow::Config::CONFIG_FILE_PATH}"
       GitReflow.say "Created #{GitReflow::Config::CONFIG_FILE_PATH} for Reflow specific configurations", :notice
-      GitReflow::Config.add "include.path", GitReflow::Config::CONFIG_FILE_PATH
+      GitReflow::Config.add "include.path", GitReflow::Config::CONFIG_FILE_PATH, global: true
       GitReflow.say "Added #{GitReflow::Config::CONFIG_FILE_PATH} to ~/.gitconfig include paths", :notice
     end
 

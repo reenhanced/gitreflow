a82ccd5e42f150e95c33a46de5361971d42f3ab2
Simon Zhu <simonzhu24@gmail.com>
2016-04-28 12:06:57 -0700

    [Issue #66] Always have reflow start create a branch from master

--- lib/git_reflow/commands/start.rb
+++ lib/git_reflow/commands/start.rb
@@ -1,17 +1,25 @@
+
 desc 'Start will create a new feature branch and setup remote tracking'
 long_desc <<LONGTIME
   Performs the following:\n
-  \t$ git pull origin <current_branch>\n
-  \t$ git push origin <current_branch>:refs/heads/[new_feature_branch]\n
+  \t$ git checkout <base_branch>\n
+  \t$ git pull origin <base_branch>\n
+  \t$ git push origin <base_branch>:refs/heads/[new_feature_branch]\n
   \t$ git checkout --track -b [new_feature_branch] origin/[new_feature_branch]\n
 LONGTIME
 arg_name '[new-feature-branch-name] - name of the new feature branch'
 command :start do |c|
+  c.flag [:b,:base], default_value: 'master'
   c.action do |global_options, options, args|
     if args.empty?
       raise "usage: git-reflow start [new-branch-name]"
     else
-      `git pull origin #{GitReflow.current_branch}`
+      # base_branch is the branch that you want to base your feature branch off of
+      # This command allows you to 'git reflow start' off your base branch
+      base_branch = options[:base]
+  
+      `git checkout #{base_branch}`
+      `git pull origin #{base_branch}`
       `git push origin #{GitReflow.current_branch}:refs/heads/#{args[0]}`
       `git checkout --track -b #{args[0]} origin/#{args[0]}`
     end

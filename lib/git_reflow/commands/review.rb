06c74f46b33a5959cf65ca009fcc27d0052838c6
Valentino Stoll <thevalentino+code@gmail.com>
2016-05-04 11:04:02 -0400

    Remove default values for review flags 'title' and 'body'

--- lib/git_reflow/commands/review.rb
+++ lib/git_reflow/commands/review.rb
@@ -3,8 +3,8 @@ arg_name 'Describe arguments to review here'
 command :review do |c|
   c.desc 'push your latest feature branch changes to your remote repo and create a pull request against the destination branch'
   c.arg_name '[destination_branch] - the branch you want to merge your feature branch into'
-  c.flag [:t, :title], default_value: 'last commit message'
-  c.flag [:m, :message], default_value: 'title'
+  c.flag [:t, :title]
+  c.flag [:m, :message]
   c.action do |global_options,options,args|
     if options[:title] || options[:message]
       review_options = {

759f3c3d90fee07ba37f3b97bf0cd7809382a71c
timraasveld <ttimitri@gmail.com>
2016-06-07 11:26:17 +0200

    ENV in all caps

--- spec/lib/git_reflow/git_helpers_spec.rb
+++ spec/lib/git_reflow/git_helpers_spec.rb
@@ -21,7 +21,7 @@ describe GitReflow::GitHelpers do
 
   describe '.git_editor_command' do
     subject { Gitacular.git_editor_command }
-    before { ENV['editor'] = 'vim' }
+    before { ENV['EDITOR'] = 'vim' }
 
     it 'defaults to GitReflow config' do
       allow(GitReflow::Config).to receive(:get).with('core.editor').and_return 'nano'

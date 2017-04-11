ac4fabbdbd32d87fc4681887abdb01a658126884
Valentino <codenamev@users.noreply.github.com>
2017-04-11 10:22:19 -0400

    Updates Github merge request to use new `merge_method` param
    This allows users to now use any merge method provided by Github: `merge`, `squash`, or `rebase` :smile:
    Merges #211
    Fixes #52
    
    LGTM given by: @nhance

--- lib/git_reflow/git_server/git_hub/pull_request.rb
+++ lib/git_reflow/git_server/git_hub/pull_request.rb
@@ -118,6 +118,9 @@ module GitReflow
 
               options[:body] = "#{options[:message]}\n" if options[:body].nil? and "#{options[:message]}".length > 0
 
+              merge_method   = options[:merge_method] || GitReflow::Config.get("reflow.merge-method")
+              merge_method   = "squash" if "#{merge_method}".length < 1
+
               merge_response = GitReflow::GitServer::GitHub.connection.pull_requests.merge(
                 "#{GitReflow.git_server.class.remote_user}",
                 "#{GitReflow.git_server.class.remote_repo_name}",
@@ -126,7 +129,7 @@ module GitReflow
                   "commit_title"   => "#{options[:title]}",
                   "commit_message" => "#{options[:body]}",
                   "sha"            => "#{self.head.sha}",
-                  "merge_method"   => !(options[:squash] == false) ? "squash" : "merge"
+                  "merge_method"   => merge_method
                 }
               )
 
